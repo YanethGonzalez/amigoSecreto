3b7801c7f95e44d72795d26f2101f38814b88470
Yaneth Gonzalez <yanethgonzalez680@gmail.com>
2025-03-01 10:40:51 -0500

    se crea funcion agregarAmigo y limpiarCaja

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo () {
+   let txtAmigo = document.getElementById("amigo").value;
+   if (txtAmigo == ""){
+    alert("por favor ingresa un nombre");
+   }
+   else {
+   amigos.push (txtAmigo); 
+   console.log(amigos);
+   limpiarCaja();
+
+   }   
+}
+
+function limpiarCaja () {
+   document.getElementById("amigo").value = '';
+}
