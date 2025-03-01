eaa37a656a647066da64f25b1d29d05313742beb
Yaneth Gonzalez <yanethgonzalez680@gmail.com>
2025-03-01 11:06:10 -0500

    se agrega descripcion

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+//Adiciona amigos a la lista
 function agregarAmigo () {
    let txtAmigo = document.getElementById("amigo").value;
    if (txtAmigo == ""){
